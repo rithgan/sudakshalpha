3ca23080c074902f95fee19452432ace496cb917
rithgan <rithikgandhi1989@gmail.com>
2020-05-16 00:19:35 +0530

    ui and bug fix

---
+++ js/scroll.js
@@ -0,0 +1,28 @@
+
+function about(){
+    let elmnt = document.getElementById("about");
+    elmnt.scrollIntoView({
+      behavior:"smooth"
+    });
+  }
+
+function project(){
+  let elmnt = document.getElementById("project");
+  elmnt.scrollIntoView({
+    behavior:"smooth"
+  });
+}
+
+function testimonial(){
+  let elmnt = document.getElementById("testimonial");
+elmnt.scrollIntoView({
+  behavior:"smooth"
+});
+}
+
+function donate(){
+  let elmnt = document.getElementById("donate");
+  elmnt.scrollIntoView({
+    behavior:"smooth"
+  });
+}
